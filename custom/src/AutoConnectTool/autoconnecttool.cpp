a31149aea293a9a934da0f70c711897bb5ab31b4
sirius <sirius2beta@gmail.com>
2023-10-27 16:28:04 +0800

    add linkList add, delete, edit function to AutoConnectManager

--- custom/src/AutoConnectTool/autoconnecttool.cpp
+++ custom/src/AutoConnectTool/autoconnecttool.cpp
@@ -14,14 +14,42 @@ AutoConnectTool::~AutoConnectTool()
 
 }
 
-QString AutoConnectTool::targetIP()
+bool AutoConnectTool::addLink(SharedLinkInterfacePtr link, QString ip)
 {
-    return _targetIP;
+    if(!_linkList.contains(link)){
+        _linkList.insert(link, ip);
+        qDebug()<<"AutoConnectTool::addLink, IP:"<<ip;
+    }else{
+        return false;
+    }
+    return true;
+}
+
+bool AutoConnectTool::deleteLink(SharedLinkInterfacePtr link)
+{
+    if(!_linkList.contains(link)){
+        _linkList.remove(link);
+        qDebug()<<"AutoConnectTool::deleteLink";
+    }else{
+        return false;
+    }
+    return true;
+}
+
+bool AutoConnectTool::editLink(SharedLinkInterfacePtr link, QString ip)
+{
+    if(!_linkList.contains(link)){
+        _linkList[link] = ip;
+    }else{
+        return false;
+    }
+    return true;
 }
 
 void AutoConnectTool::setTargetIP(QString ip)
 {
     _targetIP = ip;
+    emit targetIPChanged(ip);
 }
 
 void AutoConnectTool::connect(){
--- custom/src/AutoConnectTool/autoconnecttool.h
+++ custom/src/AutoConnectTool/autoconnecttool.h
@@ -2,7 +2,11 @@
 #define AUTOCONNECTTOOL_H
 
 #include <QObject>
+#include <QMap>
 #include "QGCToolbox.h"
+#include "LinkInterface.h"
+
+class LinkInterface;
 
 class AutoConnectTool : public QObject
 {
@@ -11,16 +15,21 @@ public:
     Q_PROPERTY(QString targetIP READ targetIP WRITE setTargetIP NOTIFY targetIPChanged)
 
     Q_INVOKABLE void connect();
+    Q_INVOKABLE bool addLink(SharedLinkInterfacePtr link, QString ip);
+    Q_INVOKABLE bool deleteLink(SharedLinkInterfacePtr link);
+    Q_INVOKABLE bool editLink(SharedLinkInterfacePtr link, QString ip);
+
     explicit AutoConnectTool(QObject *parent = nullptr);
     ~AutoConnectTool();
     void setToolBox(QGCToolbox* toolbox){   _toolbox = toolbox;};
-    QString targetIP();
+    QString targetIP()  {    return _targetIP;  };
     void setTargetIP(QString ip);
 signals:
     void targetIPChanged(QString ip);
 private:
     QString _targetIP;
     QGCToolbox* _toolbox;
+    QMap<SharedLinkInterfacePtr, QString> _linkList;
 
 };
 
